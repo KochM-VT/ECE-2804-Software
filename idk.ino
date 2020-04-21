94d4c4c45828df45b1021271398a0a10dcae1f08
blittzer <60242509+blittzer@users.noreply.github.com>
2020-04-21 10:09:33 -0400

    Add files via upload
    I havent fully figured out what pins its going through and havent been able to the test the ability to read the data, but the math is correct

---
+++ idk.ino
@@ -0,0 +1,72 @@
+// This number is subject to change depending on final product
+// This is a placeholder
+double btwnSpeakers = .01;
+int micA = 0;
+int micB = 0;
+// This is speed/frequency assuming ideal conditions
+double wavelength = 0.343;
+double timeone;
+double timetwo;
+double diff;
+double d;
+double argument;
+double rad;
+double angle;
+double conversion = 180/PI;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(1, INPUT);
+  pinMode(2, INPUT);
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // This part will change depending on how we interface with the circuit
+  if (micA > 1)
+  {
+    if (analogRead(A0) == HIGH)
+    {
+      timeone = micros();
+      micA = 1;
+    }    
+  }
+  if (micB > 1)
+  {
+    if (analogRead(A1) == HIGH)
+    {
+      timetwo = micros();
+      micB = 1;
+    }
+  }
+  // This is the math that works and we will create a diagram labeling microphone A and B
+  // It accurately determines the angle we just need to figure out a good way to figure out
+  // the time that the wave hit each of microphones.
+  if (timeone > timetwo)
+  {
+    diff = timeone - timetwo;
+    d = diff * wavelength;
+    argument = d/btwnSpeakers;
+    rad = acos(argument);
+    angle = rad * conversion;
+    Serial.println("The speakers is closer to microphone A");
+    Serial.print("The speaker is located ");
+    Serial.print(angle);
+    Serial.println(" degrees relative to microphone B.");
+  }
+  else if (timetwo > timeone) 
+  {
+    diff = timetwo - timeone;
+    d = diff * wavelength;
+    argument = d/btwnSpeakers;
+    rad = acos(argument);
+    angle = rad * conversion;
+    Serial.println("The speakers is closer to microphone B");
+    Serial.print("The speaker is located ");
+    Serial.print(angle);
+    Serial.println(" degrees relative to microphone A.");
+  }
+    
+}
